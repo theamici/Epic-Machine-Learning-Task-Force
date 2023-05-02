42a42f45a6868543a8f461e634c2d7caae704e5a
TheAmici <amici@posteo.net>
2023-05-02 15:53:41 +0200

    updated word_count_notebook.py

--- word_count_notebook.py
+++ word_count_notebook.py
@@ -2,16 +2,19 @@ import io
 import os
 import json
 
-fname = "FinalDelivery.ipynb"
-pname = os.path.dirname(__file__) + "\\" + fname
-print(pname)
+while(True):
+    name = input("Jupyter file name WITHOUT extension\n")
+    extension = ".ipynb"
+    fname = name + extension
+    pname = os.path.dirname(__file__) + "\\" + fname
+    print(pname)
 
-f = open(pname, "r")
-json_file = json.load(f)
+    f = open(pname, "r")
+    json_file = json.load(f)
 
-word_count = 0
-for cell in json_file["cells"]:
-    if cell["cell_type"] == "markdown":
-        for line in cell["source"]:
-            word_count += len(line.replace("#", "").lstrip().split(" "))
-print(f"Number of words: {word_count}")
+    word_count = 0
+    for cell in json_file["cells"]:
+        if cell["cell_type"] == "markdown":
+            for line in cell["source"]:
+                word_count += len(line.replace("#", "").lstrip().split(" "))
+    print(f"Number of words: {word_count}\n\n")
